6bca18c8ebf76fb8919677c57e325d60d3374c37
dguzvel566 <dguzvel566@g.educaand.es>
2021-11-08 21:23:20 +0100

    Commit Finalizado Ejercicio 01

---
+++ ejercicios/ejercicio01/includes/conexion.php
@@ -0,0 +1,45 @@
+<?php
+
+    try{
+
+        //Creamos la Base de Datos
+        $base = new PDO('mysql:host=localhost','root','');
+        $sql = "CREATE DATABASE IF NOT EXISTS bdusuarios";
+        $base->exec($sql);
+
+        //Nos conectamos a la Base de Datos
+        $base = new PDO('mysql:host=localhost; dbname=bdusuarios','root','');
+        
+        //Creamos una Tabla
+        $sql = "CREATE TABLE IF NOT EXISTS Usuarios(
+
+                    usuario_id int(255) auto_increment not null,
+                    nombre  varchar(50),
+                    apellidos   varchar(255),
+                    bio text,
+                    email   varchar(255),
+                    password    varchar(255),
+                    imagen  varchar(255),
+                    CONSTRAINT pk_usuario PRIMARY KEY (usuario_id)
+
+                );";
+        $base->exec($sql);
+
+        $base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo '<div class="alert alert-success text-center">La conexión a la Base de Datos se ha realizado correctamente :)</div>';
+
+    }catch (PDOException $e){
+
+        die(
+            '<div class="alert alert-danger text-center">
+                No se ha podido conectar a la Base de Datos :( <br>'.$e->getMessage().'
+            </div>'
+        );
+
+    }finally{
+
+        $base = null;
+
+    }
+
+?>
