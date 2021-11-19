d367898e939f53b1a63ab7b551bf0181bf5e4378
dguzvel566 <dguzvel566@g.educaand.es>
2021-11-20 00:24:03 +0100

    Commit Finalizado Ejercicio 05

---
+++ ejercicios/ejercicio05/includes/borrar.php
@@ -0,0 +1,48 @@
+<main>
+    <section class="container cuerpo text-center">
+        
+        <button type="button" class="btn btn-info" onclick="location.href='./index.php'">Inicio</button>
+
+        <h3 id="titulo">Eliminar Usuario</h3>
+        <?php 
+
+            if(isset($_GET["usuario_id"])&&(is_numeric($_GET["usuario_id"]))){
+
+                $usuario_id = $_GET["usuario_id"];
+
+                try{
+
+                    //Nos conectamos a la Base de Datos
+                    $base = new PDO('mysql:host=localhost; dbname=bdusuarios','root','');
+                    $base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                    
+                    //Consultar datos de una base de datos
+                    $sql = "DELETE FROM Usuarios WHERE usuario_id = :usuario_id;";
+                    $query = $base->prepare($sql);
+                    $query->execute(['usuario_id' => $usuario_id]);
+                    
+                    if($query){
+                        //echo '<div class="alert alert-success text-center">El usuario se ha eliminado correctamente :)</div>';
+                        header("Location:index.php");
+                    }
+    
+                }catch (PDOException $e){
+    
+                    die(
+                        '<div class="alert alert-danger text-center">
+                            No se ha podido acceder al usuario que desea eliminar :( <br>'.$e->getMessage().'
+                        </div>'
+                    );
+    
+                }finally{
+    
+                    $base = null;
+    
+                }  
+
+            } 
+
+        ?>
+
+    </section>
+</main>
---
+++ ejercicios/ejercicio05/includes/tabla.php
@@ -0,0 +1,22 @@
+<table class="table table-dark table-striped table-hover">
+  <tr>
+      <th>NOMBRE</th>
+      <th>E-MAIL</th>
+      <th>OPERACIONES</th>
+  </tr>
+
+  <?php while($row = $query->fetch()){ { ?> 
+
+    <tr>
+    <td><?=$row["nombre"]." ".$row["apellidos"]?></td>
+    <td><?=$row["email"]?></td>
+    <td><a href="./editar.php?usuario_id=<?=$row["usuario_id"]?>">Editar</a>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="./añadir.php">Añadir</a>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="./eliminar.php?usuario_id=<?=$row["usuario_id"]?>">Eliminar</a></td>
+    </tr>
+
+  <?php } } ?>
+  
+</table>
